4e2c3ecdb81de54ef8194e63653a62567d9335a7
sirmmy0002 <sammyndungu52@gmail.com>
2022-06-05 10:48:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// const body = document.body
+const main = document.getElementById('main')
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.body.appendChild(newHeader)
